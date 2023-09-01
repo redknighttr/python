e9a0883570c621eb1a97baf8970ca4b403179e2b
redknighttr <92789255+redknighttr@users.noreply.github.com>
2023-09-01 16:05:04 +0300

    Calcunoter.py
    It is a standart calculator i just put noter in code

--- bestversion.py
+++ bestversion.py
@@ -1,5 +1,3 @@
-Python 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 >>> def view_notes():
 ...     try:
 ...         with open("calculator_notes.txt", "r") as file:
